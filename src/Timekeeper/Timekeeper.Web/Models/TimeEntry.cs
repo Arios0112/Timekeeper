42e97d91155767ed88e96d58b190c7cfa45028f4
Andrea Valentina Ojeda Rios <valentinaojeda0112@gmail.com>
2024-11-09 19:13:02 +0100

    Starts Table for time management

---
+++ src/Timekeeper/Timekeeper.Web/Models/TimeEntry.cs
@@ -0,0 +1,12 @@
+﻿namespace Timekeeper.Web.Models;
+
+public class TimeEntry
+{
+    public required string Name { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public string Comment { get; set; } = string.Empty;
+    public bool IsSickLeave { get; set; }
+
+    public TimeSpan Duration => (EndDate ?? DateTime.UtcNow) - StartDate;
+}
